51b4a986ebb3f99061f2f584348e0efa71d5806a
Chris Gillum <cgillum@microsoft.com>
2022-06-09 15:14:11 -0700

    Durable timers and logging (#4)

---
+++ src/Helpers.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Logging;
+
+namespace DurableTask.Dapr;
+
+static class Helpers
+{
+    public static async Task ParallelForEachAsync<T>(this IList<T> items, int maxConcurrency, Func<T, Task> action)
+    {
+        if (items.Count == 0)
+        {
+            return;
+        }
+
+        static async Task InvokeThrottledAction(T item, Func<T, Task> action, SemaphoreSlim semaphore)
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                await action(item);
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+        using var semaphore = new SemaphoreSlim(maxConcurrency);
+        Task[] tasks = new Task[items.Count];
+        for (int i = 0; i < items.Count; i++)
+        {
+            tasks[i] = InvokeThrottledAction(items[i], action, semaphore);
+        }
+
+        await Task.WhenAll(tasks);
+    }
+
+    public static Task ParallelForEachAsync<T>(this IList<T> items, Func<T, Task> action)
+    {
+        // Choosing a max concurrency is a tradeoff between throughput and the overhead of thread creation.
+        // A conservative value of 4 feels like a safe default.
+        return ParallelForEachAsync(items, maxConcurrency: 4, action);
+    }
+
+    public static ILogger CreateLoggerForDaprProvider(this ILoggerFactory factory)
+    {
+        return factory.CreateLogger("DurableTask.Dapr");
+    }
+
+    public static TimeSpan PositiveOrZero(this TimeSpan timeSpan)
+    {
+        return timeSpan > TimeSpan.Zero ? timeSpan : TimeSpan.Zero;
+    }
+}
--- src/OrchestrationActor.cs
+++ src/OrchestrationActor.cs
@@ -1,30 +1,33 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
 using Dapr.Actors.Runtime;
 using DurableTask.Core;
 using DurableTask.Core.History;
+using Microsoft.Extensions.Logging;
 
 namespace DurableTask.Dapr;
 
 // NOTE: This code runs on the container where the app code lives
 class DaprWorkflowScheduler : Actor, IOrchestrationActor, IRemindable
 {
-    const string OrchestrationReminder = "background-execute";
-
     readonly IWorkflowScheduler workflowScheduler;
+    readonly ILogger log;
 
-    InternalState? state;
+    WorkflowState? state;
 
-    public DaprWorkflowScheduler(ActorHost host, IWorkflowScheduler workflowScheduler)
+    public DaprWorkflowScheduler(ActorHost host, IWorkflowScheduler workflowScheduler, ILoggerFactory loggerFactory)
         : base(host)
     {
         this.workflowScheduler = workflowScheduler ?? throw new ArgumentNullException(nameof(workflowScheduler));
+        this.log = loggerFactory.CreateLoggerForDaprProvider();
     }
 
     async Task IOrchestrationActor.InitAsync(TaskMessage creationMessage, OrchestrationStatus[] dedupeStatuses)
     {
-        ConditionalValue<InternalState> existing = await this.TryGetStateAsync();
+        ConditionalValue<WorkflowState> existing = await this.TryLoadStateAsync();
 
         // NOTE: Validation must happen before the reminder is scheduled.
         // De-dupe logic
@@ -46,11 +49,11 @@ class DaprWorkflowScheduler : Actor, IOrchestrationActor, IRemindable
         // RELIABILITY:
         // This reminder must be scheduled before the state update to ensure that an unexpected process failure
         // doesn't result in orphaned state in the state store.
-        await this.CreateOrchestratorReminder(delay: TimeSpan.Zero);
+        await this.CreateOrchestratorReminder("init", delay: TimeSpan.Zero);
 
         // Cache the state in memory so that we don't have to fetch it again when the reminder fires.
         DateTime now = DateTime.UtcNow;
-        this.state = new InternalState
+        this.state = new WorkflowState
         {
             OrchestrationStatus = OrchestrationStatus.Pending,
             Name = startEvent.Name,
@@ -71,18 +74,39 @@ class DaprWorkflowScheduler : Actor, IOrchestrationActor, IRemindable
         await this.StateManager.SaveStateAsync();
     }
 
-    async Task IRemindable.ReceiveReminderAsync(string reminderName, byte[] state, TimeSpan dueTime, TimeSpan period)
+    async Task IRemindable.ReceiveReminderAsync(
+        string reminderName,
+        byte[] reminderState,
+        TimeSpan dueTime,
+        TimeSpan period)
     {
-        if (this.state == null)
-        {
-            // The actor may have been unloaded from memory. Refetch it's internal state.
-            this.state = await this.StateManager.GetStateAsync<InternalState>("state");
-        }
+        this.log.ReminderFired(this.Id.ToString(), reminderName);
 
+        await this.TryLoadStateAsync();
         if (this.state == null)
         {
             // The actor may have failed to save its state after being created. The client should have already received
             // an error message associated with the failure, so we can safely drop this reminder.
+            this.log.WorkflowStateNotFound(this.Id.ToString(), reminderName);
+            return;
+        }
+
+        if (IsReminderForDurableTimer(reminderName))
+        {
+            if (this.state.Timers.TryGetValue(reminderName, out TimerFiredEvent? timerEvent))
+            {
+                this.state.Inbox.Add(this.state.NewMessage(timerEvent));
+            }
+            else
+            {
+                this.log.TimerNotFound(this.state.InstanceId, reminderName);
+            }
+        }
+
+        if (!this.state.Inbox.Any())
+        {
+            // Nothing to do! This isn't expected.
+            this.log.InboxIsEmpty(this.state.InstanceId, reminderName);
             return;
         }
 
@@ -106,27 +130,54 @@ class DaprWorkflowScheduler : Actor, IOrchestrationActor, IRemindable
                 {
                     this.state.CompletedTimeUtc = utcNow;
                 }
+                else
+                {
+                    // Schedule reminders for durable timers
+                    await result.Timers.ParallelForEachAsync(item =>
+                    {
+                        TimerFiredEvent timer = (TimerFiredEvent)item.Event;
+                        lock (this.state.Timers)
+                        {
+                            this.state.Timers.Add(timer);
+                        }
 
+                        TimeSpan reminderDelay = timer.FireAt.Subtract(utcNow).PositiveOrZero();
+                        return this.CreateOrchestratorReminder(GetReminderNameForTimer(timer), reminderDelay);
+                    });
+
+                    // TODO: Process outbox messages
+                }
+
+                // At this point, the inbox should be fully processed.
+                this.state.Inbox.Clear();
+
+                // Append the new history
+                this.state.History.AddRange(result.NewHistoryEvents);
+
+                // RELIABILITY:
+                // Saving state needs to be the last step to ensure that a crash doesn't cause us to lose any work.
+                // If there is a crash, there should be a reminder that wakes us up and causes us to reprocess the
+                // latest orchestration state.
+                // TODO: Need to implement this persistent reminder...
                 await this.StateManager.SaveStateAsync();
 
-                // TODO: Process the items in the outbox
                 break;
             case WorkflowExecutionResultType.Throttled:
                 // TODO: Exponential backoff with some randomness to avoid thundering herd problem.
-                await this.CreateOrchestratorReminder(delay: TimeSpan.FromSeconds(30));
+                await this.CreateOrchestratorReminder("retry", delay: TimeSpan.FromSeconds(30));
                 break;
             case WorkflowExecutionResultType.Abandoned:
-                await this.CreateOrchestratorReminder(delay: TimeSpan.FromSeconds(5));
+                await this.CreateOrchestratorReminder("retry", delay: TimeSpan.FromSeconds(5));
                 break;
         }
     }
 
     async Task<OrchestrationState?> IOrchestrationActor.GetCurrentStateAsync()
     {
-        ConditionalValue<InternalState> existing = await this.TryGetStateAsync();
+        ConditionalValue<WorkflowState> existing = await this.TryLoadStateAsync();
         if (existing.HasValue)
         {
-            InternalState internalState = existing.Value;
+            WorkflowState internalState = existing.Value;
             return new OrchestrationState
             {
                 CompletedTime = internalState.CompletedTimeUtc.GetValueOrDefault(),
@@ -150,16 +201,17 @@ class DaprWorkflowScheduler : Actor, IOrchestrationActor, IRemindable
         return null;
     }
 
-    async Task<ConditionalValue<InternalState>> TryGetStateAsync()
+    async Task<ConditionalValue<WorkflowState>> TryLoadStateAsync()
     {
         // Cache hit?
         if (this.state != null)
         {
-            return new ConditionalValue<InternalState>(true, this.state);
+            return new ConditionalValue<WorkflowState>(true, this.state);
         }
 
         // Cache miss
-        ConditionalValue<InternalState> result = await this.StateManager.TryGetStateAsync<InternalState>("state");
+        this.log.FetchingWorkflowState(this.Id.ToString());
+        ConditionalValue<WorkflowState> result = await this.StateManager.TryGetStateAsync<WorkflowState>("state");
         if (result.HasValue)
         {
             this.state = result.Value;
@@ -168,29 +220,73 @@ class DaprWorkflowScheduler : Actor, IOrchestrationActor, IRemindable
         return result;
     }
 
-    Task CreateOrchestratorReminder(TimeSpan delay)
+    Task CreateOrchestratorReminder(string reminderName, TimeSpan delay)
     {
-        // All orchestrator reminders have the same name and are non-repeating
-        return this.RegisterReminderAsync(
-            OrchestrationReminder,
-            null,
-            delay,
-            TimeSpan.FromMilliseconds(-1));
+        // Non-repeating reminder
+        TimeSpan periodForNoRecurrence = TimeSpan.FromMilliseconds(-1);
+
+        this.log.CreatingReminder(this.Id.ToString(), reminderName, delay, periodForNoRecurrence);
+        return this.RegisterReminderAsync(reminderName, null, delay, periodForNoRecurrence);
     }
 
-    class InternalState
+    static string GetReminderNameForTimer(TimerFiredEvent e)
+    {
+        // The TimerId is unique for every timer event for a particular orchestration in the Durable Task Framework.
+        // WARNING: Do not change this naming convention since that could put timers out of sync with existing reminders.
+        return $"timer-{e.TimerId}";
+    }
+
+    static bool IsReminderForDurableTimer(string reminderName)
+    {
+        return reminderName.StartsWith("timer-");
+    }
+
+    /// <summary>
+    /// A collection of pending durable timers that gets saved into workflow state.
+    /// </summary>
+    /// <remarks>
+    /// This collection contains one record for every durable timer scheduled by a specific orchestration instance.
+    /// This collection supports O(1) key-based lookups, where the key is the name of the associated reminder in Dapr.
+    /// </remarks>
+    class TimerCollection : KeyedCollection<string, TimerFiredEvent>
+    {
+        protected override string GetKeyForItem(TimerFiredEvent e)
+        {
+            return GetReminderNameForTimer(e);
+        }
+    }
+
+    class WorkflowState
     {
         public OrchestrationStatus OrchestrationStatus { get; set; }
         public string Name { get; init; } = "";
         public string InstanceId { get; init; } = "";
         public string ExecutionId { get; init; } = "";
+        [DataMember(EmitDefaultValue = false)]
         public string? Input { get; init; }
+        [DataMember(EmitDefaultValue = false)]
         public string? Output { get; set; }
+        [DataMember(EmitDefaultValue = false)]
         public string? CustomStatus { get; set; }
         public DateTime CreatedTimeUtc { get; init; }
         public DateTime LastUpdatedTimeUtc { get; set; }
+        [DataMember(EmitDefaultValue = false)]
         public DateTime? CompletedTimeUtc { get; set; }
         public List<TaskMessage> Inbox { get; } = new List<TaskMessage>();
+        public TimerCollection Timers { get; } = new();
         public List<HistoryEvent> History { get; } = new List<HistoryEvent>();
+
+        internal TaskMessage NewMessage(HistoryEvent e)
+        {
+            return new TaskMessage
+            {
+                Event = e,
+                OrchestrationInstance = new OrchestrationInstance
+                {
+                    InstanceId = this.InstanceId,
+                    ExecutionId = this.ExecutionId,
+                },
+            };
+        }
     }
 }
